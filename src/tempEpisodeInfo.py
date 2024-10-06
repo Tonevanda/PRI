31407d1851a539567cec0e83ded491b2e85c3dcb
dratomitoma <64918169+dratomitoma@users.noreply.github.com>
2024-10-06 19:51:04 +0100

    episode scrape done

--- src/tempEpisodeInfo.py
+++ src/tempEpisodeInfo.py
@@ -9,25 +9,25 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-
-mw_content_text = soup.find_all(class_=["mw-body-content","mw-content-ltr"])
-
-mw_parser_output = mw_content_text[0].find_all(class_="mw-parser-output")
-
-h2_list = mw_parser_output[0].find_all("h2")
-
+long_summary_item = soup.find(lambda tag: tag.name == "h2" and tag.text == "Long Summary[]")
 content_dict={}
 
-for h2 in h2_list:
-    sibling = h2.find_next_sibling()
-    while sibling.name == "p" or sibling.name == "ul":
-        if sibling.name == "ul":
-            for characters in sibling.find_all("a"):
-                content_dict.update({h2.text:characters.text})
-            sibling=sibling.find_next_sibling()
-        else:
-            content_dict.update({h2.text:sibling.text})
-            sibling=sibling.find_next_sibling()
+sibling = long_summary_item.find_next_sibling()
+long_summary_text = []
+while sibling.name == "p":
+    long_summary_text.append(sibling.text)
+    sibling = sibling.find_next_sibling()
+content_dict["Long Summary"] = "\n".join(long_summary_text)
+
+air_date = soup.find(lambda tag: tag.name == "h3" and tag.text == "Airdate").find_next_sibling()
+content_dict["Airdate"] = air_date.text
+
+opening_ending = air_date.parent.parent.find_next_sibling().find_all("a")
+content_dict["Opening"] = opening_ending[0].text
+content_dict["Ending"] = opening_ending[1].text
+
+season_piece=air_date.parent.parent.find_next_sibling().find_next_sibling().find_all("td")
+content_dict["Season"] = season_piece[0].text
+content_dict["Piece"] = season_piece[1].text
 
 print(content_dict)
-
