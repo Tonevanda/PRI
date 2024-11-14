32cea03a2b829f4784b7ce089587821f317572cf
Tiago Cruz <up202108810@up.pt>
2024-11-14 16:01:21 +0000

    bounty

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,7 +1,7 @@
-python ../src/scripts/query_solr.py --query queries/query_diable_jambe.json --uri http://localhost:8983/solr --collection episodes | \
+python ../src/scripts/query_solr.py --query queries/query_bounty.json --uri http://localhost:8983/solr --collection episodes | \
 python ../src/scripts/solr2trec.py > results_sys1_trec.txt
 
-cat ../qrels/diable_jambe.txt | python ../src/scripts/qrels2trec.py > qrels_trec.txt
+cat ../qrels/bounty.txt | python ../src/scripts/qrels2trec.py > qrels_trec.txt
 
 ../src/trec_eval/trec_eval qrels_trec.txt results_sys1_trec.txt
 
