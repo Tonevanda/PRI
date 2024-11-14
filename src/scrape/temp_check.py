05b4a9dc53bff1c5b3d69510d4cd3796f236b696
Tomás Xavier <tomasxavier03@gmail.com>
2024-11-14 19:27:53 +0000

    continuing subtitle scrape on other pc

---
+++ src/scrape/temp_check.py
@@ -0,0 +1,22 @@
+import os
+
+def find_missing_files(directory, start, end):
+    existing_files = set()
+    for filename in os.listdir(directory):
+        if filename.endswith('.ass'):
+            try:
+                number = int(filename.split('.')[0])
+                if start <= number <= end:
+                    existing_files.add(number)
+            except ValueError:
+                continue
+
+    missing_files = [num for num in range(start, end + 1) if num not in existing_files]
+    return missing_files
+
+if __name__ == "__main__":
+    directory = 'manual_batch'
+    start = 937
+    end = 1100
+    missing_files = find_missing_files(directory, start, end)
+    print("Missing files:", missing_files)
