0adcd1571fd0c3150f0e5f7d84d37a2ca5c607f0
Tiago Cruz <tiagocruz057@gmail.com>
2024-11-15 11:26:29 +0000

    changed bounty query and added default startup with default schema for comparison

--- defstartup.sh
+++ defstartup.sh
@@ -6,20 +6,11 @@ docker run --rm -p 8983:8983 --name onepiece_solr -v "${PWD}:/data" -d solr:9 so
 # Add a delay to ensure Solr is fully up and running
 sleep 5
 
-# Update synonyms.txt
-docker cp ./data/synonyms.txt onepiece_solr:/var/solr/data/episodes/conf/synonyms.txt
-
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
-    --data-binary "@./data/schema.json" \
+    --data-binary "@./data/default_schema.json" \
     http://localhost:8983/solr/episodes/schema
 
-# Add params
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary "@./data/params.json" \
-    http://localhost:8983/solr/episodes/config/params
-
-# Index data
 curl -X POST -H 'Content-type:text/csv' \
     --data-binary "@./data/data.csv" \
     http://localhost:8983/solr/episodes/update?commit=true
