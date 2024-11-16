08c348c84ded8d97623867ec04b1e8f26463f95f
Tiago Cruz <tiagocruz057@gmail.com>
2024-11-16 03:24:14 +0000

    Organization

--- scripts/eval_ancient_weapon.sh
+++ scripts/eval_ancient_weapon.sh
@@ -1,8 +1,8 @@
 python ../src/scripts/query_solr.py --query queries/ancient_weapon.json --uri http://localhost:8983/solr --collection episodes | \
-python ../src/scripts/solr2trec.py > results_sys1_trec.txt
+python ../src/scripts/solr2trec.py > ./results/results_aw_trec.txt
 
-cat ../qrels/ancient_weapon.txt | python ../src/scripts/qrels2trec.py > qrels_trec.txt
+cat ../qrels/ancient_weapon.txt | python ../src/scripts/qrels2trec.py > ./results/qrels_trec.txt
 
-../src/trec_eval/trec_eval qrels_trec.txt results_sys1_trec.txt
+../src/trec_eval/trec_eval ./results/qrels_trec.txt ./results/results_aw_trec.txt
 
-cat results_sys1_trec.txt | python ../src/scripts/plot_pr.py --qrels qrels_trec.txt --output prec_rec_ancient_weapon.png
+cat ./results/results_aw_trec.txt | python ../src/scripts/plot_pr.py --qrels ./results/qrels_trec.txt --output ./results/prec_rec_aw.png
--- scripts/eval_def_sh_childhood.sh
+++ scripts/eval_def_sh_childhood.sh
@@ -1,8 +1,8 @@
 python ../src/scripts/query_solr.py --query queries/def_straw_hats_childhood.json --uri http://localhost:8983/solr --collection episodes | \
-python ../src/scripts/solr2trec.py > results_def_sh_childhood_trec.txt
+python ../src/scripts/solr2trec.py > ./results/results_def_sh_childhood_trec.txt
 
-cat ../qrels/straw_hats_childhood.txt | python ../src/scripts/qrels2trec.py > qrels_trec.txt
+cat ../qrels/straw_hats_childhood.txt | python ../src/scripts/qrels2trec.py > ./results/qrels_trec.txt
 
-../src/trec_eval/trec_eval qrels_trec.txt results_def_sh_childhood_trec.txt
+../src/trec_eval/trec_eval ./results/qrels_trec.txt ./results/results_def_sh_childhood_trec.txt
 
-cat results_def_sh_childhood_trec.txt | python ../src/scripts/plot_pr.py --qrels qrels_trec.txt --output prec_rec_def_childhood.png
+cat ./results/results_def_sh_childhood_trec.txt | python ../src/scripts/plot_pr.py --qrels ./results/qrels_trec.txt --output ./results/prec_rec_def_childhood.png
