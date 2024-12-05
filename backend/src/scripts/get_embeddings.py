30c9cdec46bbd38c5f5885284d39265289de7ae6
Tonevanda <up202108863@up.pt>
2024-12-05 10:04:33 +0000

    fixed embeddings

--- backend/src/scripts/get_embeddings.py
+++ backend/src/scripts/get_embeddings.py
@@ -25,11 +25,15 @@ if __name__ == "__main__":
     # Update each document in the JSON data
     for document in data:
         # Extract fields if they exist, otherwise default to empty strings
-        title = document.get("title", "")
-        objectives = document.get("objectives", "")
-        learning_outcomes = document.get("learning_outcomes", "")
+        title = document.get("Title")
+        season = document.get("Season")
+        arc = document.get("Arc")
+        saga = document.get("Saga")
+        summary = document.get("Summary")
+        anime_notes = document.get("anime notes")
+        episode_script = document.get("episode script")
 
-        combined_text = title + " " + objectives + " " + learning_outcomes
+        combined_text = title + " " + season + " " + arc + " " + saga + " " + summary + " " + anime_notes + " " + episode_script
         document["vector"] = get_embedding(combined_text)
 
     # Output updated JSON to STDOUT
