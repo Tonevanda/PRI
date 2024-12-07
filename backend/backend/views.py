b59dabb3051d2b55a6d5885b89a41f212df1fa46
Tonevanda <up202108863@up.pt>
2024-12-07 13:47:48 +0000

    added backend endpoint for episode view

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -10,10 +10,28 @@ def text_to_embedding(text):
     embedding_str = "[" + ",".join(map(str, embedding)) + "]"
     return embedding_str
 
+def episode(request, episode_id):
+    print(f"GET request for Episode {episode_id}")
+    try:
+        solr_params = {
+            "q.op": "AND",
+            "defType": "edismax",
+            "q": "*:*",
+            "fq": f"Episode: {episode_id}",
+            "wt": "json"
+        }
+
+        uri = "http://localhost:8983/solr/episodes/select"
+        response = requests.post(uri, params=solr_params, headers={"Content-Type": "application/x-www-form-urlencoded"})
+        return JsonResponse(response.json()['response']['docs'], safe=False)
+    except requests.RequestException as e:
+        print(f"Error querying Solr: {e}")
+        return JsonResponse({"error": "Error querying Solr"}, status=500)
+
 
 def search(request):
     query = request.GET.get("query", '')
-    print(query)
+    print(f"GET request for search: {query}")
     embedding = text_to_embedding(query)
     try:
         solr_data = {
