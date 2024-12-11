85f256c59e027ea82eb5ba40f3437227bbba1aa3
Tiago Cruz <tiagocruz057@gmail.com>
2024-12-11 18:50:26 +0000

    query now uses both embeddings and m2 params

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -33,6 +33,7 @@ def search(request):
     query = request.GET.get("query", '')
     print(f"GET request for search: {query}")
     embedding = text_to_embedding(query)
+
     try:
         solr_data = {
             "q": f"{{!knn f=vector topK=10}}{embedding}",
@@ -41,7 +42,7 @@ def search(request):
         }
         solr_params = {
             "q": query,
-            "rq": f"{{!rerank reRankQuery=$rqq reRankDocs=30 reRankWeight=1}}",
+            "rq": "{!rerank reRankQuery=$rqq reRankDocs=30 reRankWeight=0.2}",
             "rqq": f"{{!knn f=vector topK=10}}{embedding}",
             "useParams": "params",
             "fl": "score, *",
@@ -50,13 +51,15 @@ def search(request):
 
         url = "http://localhost:8983/solr/episodes/select"
         response_embeddings = requests.post(url=url, data=solr_data, headers={"Content-Type": "application/x-www-form-urlencoded"})
-        response_params = requests.post(url=url, params=solr_params, headers={"Content-Type": "application/x-www-form-urlencoded"})
+        response_params = requests.post(url=url, data=solr_params, headers={"Content-Type": "application/x-www-form-urlencoded"})
+
+        embeddingsJson = JsonResponse(response_embeddings.json()['response']['docs'], safe=False)
+        responseJson = JsonResponse(response_params.json()['response']['docs'], safe=False)
     except requests.RequestException as e:
         print(f"Error querying Solr: {e}")
 
 
-    embeddingsJson = JsonResponse(response_embeddings.json()['response']['docs'], safe=False)
-    responseJson = JsonResponse(response_params.json()['response']['docs'], safe=False)
+    
 
 
 
