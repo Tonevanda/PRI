06ae2b44b3c638be2c2baa867bfcacc4c6cd53a8
Tiago Cruz <tiagocruz057@gmail.com>
2024-12-14 00:49:45 +0000

    vectors as a child document

--- backend/project/src/scripts/get_embeddings.py
+++ backend/project/src/scripts/get_embeddings.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     csv_file = '../../data/data.csv' 
     #json_file = '../../data/data_embeddings.json' 
-    json_file = './summary_embeddings.json'
+    json_file = './data_embeddings.json'
 
     data = []
     with open(csv_file, mode='r', encoding='utf-8') as file:
@@ -36,39 +36,24 @@ if __name__ == "__main__":
         for row in csv_reader:
             data.append(row)
 
-
+    combined_embeddings = []
+    
     # Update each document in the JSON data
     for document in data:
         # Extract fields if they exist, otherwise default to empty strings
-        #title = document.get("Title", "")
-        #season = document.get("Season", "")
-        #arc = document.get("Arc", "")
-        #saga = document.get("Saga", "")
         summary = document.get("Summary", "")
-        #anime_notes = document.get("anime notes", "")
-        #episode_script = document.get("episode script", "")
-
-        # Generate embeddings for each field
-        #title_embedding = get_embedding(title)
-        #season_embedding = get_embedding(season)
-        #arc_embedding = get_embedding(arc)
-        #saga_embedding = get_embedding(saga)
+        document["vectors"] = []
 
         # Generate embeddings phrase by phrase for Summary, Anime Notes, and Episode Script
         summary_phrases = process_text_phrase_by_phrase(summary)
-        #anime_notes_phrases = process_text_phrase_by_phrase(anime_notes)
-        #episode_script_phrases = process_text_phrase_by_phrase(episode_script)
 
-        summary_embeddings = [get_embedding(phrase) for phrase in summary_phrases]
-        #anime_notes_embeddings = [get_embedding(phrase) for phrase in anime_notes_phrases]
-        #episode_script_embeddings = [get_embedding(phrase) for phrase in episode_script_phrases]
-
-        # Combine all embeddings into a single vector
-        combined_embeddings = (
-            [item for sublist in summary_embeddings for item in sublist]
-        )
-
-        document["vector"] = combined_embeddings
+        for phrase in summary_phrases:
+            # Generate embeddings for each phrase
+            embedding = get_embedding(phrase)
+            document["vectors"].append({
+                "vector": embedding
+            })
+    
 
     # Output updated JSON to STDOUT
     with open(json_file, mode='w', encoding='utf-8') as file:
