22ac73cf79f1311e62f1311c1d44fc0ebf66d68b
Tiago Cruz <tiagocruz057@gmail.com>
2024-12-15 18:16:33 +0000

    Fixed some mistakes

--- backend/project/src/scripts/pipeline.py
+++ backend/project/src/scripts/pipeline.py
@@ -23,14 +23,12 @@ def feedForward(query_file):
 
     for query_name, params in query_json.items():
         params['q'] = params['q'].replace('"', '\\"')
-
+        rqq = "False"
         if(not query_name.startswith("m2") and not query_name.startswith("def")):
-            embedding = text_to_embedding(params['q'])
-            params['rqq'] = '{!parent which=\"*:* -_nest_path_:*\" score=max}{!knn f=vector topK=100}' + str(embedding)
-            print(params)
-        
+            rqq = "True"
+
         command = (
-            f"python query_solr.py --query \"{params['q']}\" --collection {params['collection']} --useParams {params['useParams']} --uri http://localhost:8983/solr |"
+            f"python query_solr.py --query \"{params['q']}\" --collection {params['collection']} --useParams {params['useParams']} --uri http://localhost:8983/solr --useRqq {rqq} |"
             f"python solr2trec.py > ./results/{query_name}.txt"
         )
         subprocess.run(command, shell=True, check=True)
--- backend/project/src/scripts/query_solr.py
+++ backend/project/src/scripts/query_solr.py
@@ -8,10 +8,18 @@ from pathlib import Path
 
 import requests
 
+from sentence_transformers import SentenceTransformer
+
+def text_to_embedding(text):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    embedding = model.encode(text, convert_to_tensor=False).tolist()
+    
+    # Convert the embedding to the expected format
+    embedding_str = "[" + ",".join(map(str, embedding)) + "]"
+    return embedding_str
 
 
-
-def fetch_solr_results(query, collection, params, solr_uri):
+def fetch_solr_results(query, collection, params, solr_uri, useRqq):
     """
     Fetch search results from a Solr instance based on the query parameters.
 
@@ -19,6 +27,7 @@ def fetch_solr_results(query, collection, params, solr_uri):
     - query_file: Path to the JSON file containing Solr query parameters.
     - solr_uri: URI of the Solr instance (e.g., http://localhost:8983/solr).
     - collection: Solr collection name from which results will be fetched.
+    - useRqq: If we're meant to use the RQQ parameter.
 
     Output:
     - Prints the JSON search results to STDOUT.
@@ -27,16 +36,28 @@ def fetch_solr_results(query, collection, params, solr_uri):
     # Construct the Solr request URL
     uri = f"{solr_uri}/{collection}/select"
 
+    if(useRqq=="True"):
+        embedding = text_to_embedding(query)
+        rqq = '{!parent which=\"*:* -_nest_path_:*\" score=max}{!knn f=vector topK=100}' + str(embedding)
+
     try:
         # Send the POST request to Solr
 
-
-        
-        solr_params = {
-            "q": query,
-            "fl": "id, Episode, score",
-            "useParams": params,
-        }
+        if(rqq == "False"):
+            print("\n   rqq is empty\n")
+            solr_params = {
+                "q": query,
+                "fl": "id, Episode, score",
+                "useParams": params
+            }
+        else:
+            print("\n   rqq is not empty\n")
+            solr_params = {
+                "q": query,
+                "fl": "id, Episode, score",
+                "useParams": params,
+                "rqq": rqq
+            }
 
         response = requests.post(uri, data=solr_params, headers={"Content-Type": "application/x-www-form-urlencoded"})
         response.raise_for_status()  # Raise error if the request failed
@@ -80,9 +101,16 @@ if __name__ == "__main__":
         default="http://localhost:8983/solr",
         help="The URI of the Solr instance (default: http://localhost:8983/solr).",
     )
+    parser.add_argument(
+        "--useRqq",
+        type=str,
+        required=True,
+        default="False",
+        help="If we're meant to use the RQQ parameter.",
+    )
 
     # Parse command-line arguments
     args = parser.parse_args()
 
     # Call the function with parsed arguments
-    fetch_solr_results(args.query, args.collection, args.useParams, args.uri)
+    fetch_solr_results(args.query, args.collection, args.useParams, args.uri, args.useRqq)
